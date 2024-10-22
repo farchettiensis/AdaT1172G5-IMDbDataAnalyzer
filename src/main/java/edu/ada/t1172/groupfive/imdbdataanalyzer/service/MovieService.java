07ce78f49516f1c6a216d99b17330f513c9b6dba
victorferreiras <victor.fagundes586@gmail.com>
2024-10-21 21:10:29 -0300

    removed unused methods

--- src/main/java/edu/ada/t1172/groupfive/imdbdataanalyzer/service/MovieService.java
+++ src/main/java/edu/ada/t1172/groupfive/imdbdataanalyzer/service/MovieService.java
@@ -1,16 +1,9 @@
 package edu.ada.t1172.groupfive.imdbdataanalyzer.service;
 
 import edu.ada.t1172.groupfive.imdbdataanalyzer.model.Movie;
-import edu.ada.t1172.groupfive.imdbdataanalyzer.model.enums.Genres;
 
 import java.util.List;
 
 public interface MovieService {
     List<Movie> fetchAllMovies();
-
-    List<Movie> getMoviesByGenre(List<Movie> movies, Genres genre);
-
-    List<Movie> getMoviesByYear(List<Movie> movies, Integer year);
-
-    Movie getTopRatedMovie(List<Movie> movies);
 }
