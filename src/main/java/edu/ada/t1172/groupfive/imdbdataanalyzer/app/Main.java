23aae70e42986b6765dfbcf1130f8f98efcd3161
Nathan <nathan.llucasl@gmail.com>
2024-10-22 13:22:16 -0300

    inicializacao do bd movida

--- src/main/java/edu/ada/t1172/groupfive/imdbdataanalyzer/app/Main.java
+++ src/main/java/edu/ada/t1172/groupfive/imdbdataanalyzer/app/Main.java
@@ -3,14 +3,11 @@ package edu.ada.t1172.groupfive.imdbdataanalyzer.app;
 import edu.ada.t1172.groupfive.imdbdataanalyzer.dao.MovieDAO;
 import edu.ada.t1172.groupfive.imdbdataanalyzer.model.Movie;
 import edu.ada.t1172.groupfive.imdbdataanalyzer.model.enums.Genres;
+import edu.ada.t1172.groupfive.imdbdataanalyzer.service.H2ServerConfig;
 import edu.ada.t1172.groupfive.imdbdataanalyzer.service.MovieServiceImpl;
 import edu.ada.t1172.groupfive.imdbdataanalyzer.util.CSVParser;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 import org.h2.tools.Server;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
@@ -18,14 +15,7 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        try {
-            Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8080").start();
-            System.out.println("H2 started at http://localhost:8080");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
+        H2ServerConfig.start();
         String caminhoCSV = "src/main/resources/data.csv";
         MovieServiceImpl movieService = new MovieServiceImpl(new MovieDAO(new CSVParser(caminhoCSV)));
 
@@ -35,21 +25,18 @@ public class Main {
         List<Movie> filmes = movieService.fetchAllMoviesFromDB();
         filmes.stream().limit(20).forEach(System.out::println);
         System.out.println();
-        Movie insertTeste = new Movie("tt1843303","VelociPastor",
-                Set.of(Genres.HORROR,Genres.COMEDY),10d,1000000,2018);
+        Movie insertTeste = new Movie("tt1843303", "VelociPastor",
+                Set.of(Genres.HORROR, Genres.COMEDY), 10d, 1000000, 2018);
         movieService.saveMovie(insertTeste);
         Movie searchTeste = movieService.getMovieById("tt0111161");
         System.out.println(searchTeste);
         System.out.println();
-        Movie updateTeste = new Movie(searchTeste.getId(),"Mudando aqui pra teste",
-                searchTeste.getGenres(),0d,0,2020);
-        movieService.updateMovie("tt0111161",updateTeste);
+        Movie updateTeste = new Movie(searchTeste.getId(), "Mudando aqui pra teste",
+                searchTeste.getGenres(), 0d, 0, 2020);
+        movieService.updateMovie("tt0111161", updateTeste);
         System.out.println(movieService.getMovieById("tt0111161"));
 
         movieService.deleteMovie(movieService.getMovieById("tt0068646"));
-
-
-
 
 
     }
---
+++ src/main/java/edu/ada/t1172/groupfive/imdbdataanalyzer/service/H2ServerConfig.java
@@ -0,0 +1,17 @@
+package edu.ada.t1172.groupfive.imdbdataanalyzer.service;
+
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+
+public class H2ServerConfig {
+
+    public static void start() {
+        try {
+            Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8080").start();
+            System.out.println("H2 started at http://localhost:8080");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
